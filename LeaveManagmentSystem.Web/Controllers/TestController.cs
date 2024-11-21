59da2aa05cbdcc191e69b79d8bb96e08daecf8d1
Patrik <Muravski.patrik@gmail.com>
2024-11-21 02:08:19 +0200

    create test controler model and view

---
+++ LeaveManagmentSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagmentSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new Models.TestViewModel
+            {
+                Name = "That works",
+                DateOfBirth = new DateTime(2003, 12, 23)
+            };
+
+            return View(data);
+        }
+    }
+}
