d6fffaa31fe6a1afc1ceda7fcbaf0774aef08f12
Patrik <Muravski.patrik@gmail.com>
2024-11-23 23:18:25 +0200

    ok

--- LeaveManagmentSystem.Web/Program.cs
+++ LeaveManagmentSystem.Web/Program.cs
@@ -9,8 +9,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
