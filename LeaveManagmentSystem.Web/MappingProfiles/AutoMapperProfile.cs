ed278553c35ac16372a27b49594040f9c9950dad
Patrik <Muravski.patrik@gmail.com>
2024-12-09 17:25:09 +0200

    added AutoMapper and ViewModel for leave type

---
+++ LeaveManagmentSystem.Web/MappingProfiles/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LeaveManagmentSystem.Web.Data;
+using LeaveManagmentSystem.Web.Models.LeaveTypes;
+
+namespace LeaveManagmentSystem.Web.MappingProfiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<LeaveType, IndexVM>()
+                .ForMember(dest => dest.Days, opt => opt
+                .MapFrom(src => src.DefaultDays));
+        }
+    }
+}
