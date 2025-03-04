60607aeadf41f6dba4e7f04c66717c0103607f2e
Patrik <Muravski.patrik@gmail.com>
2025-03-05 00:08:15 +0200

    moved database connection to serviceRegistrationClass

--- LeaveManagmentSystem.Web/Program.cs
+++ LeaveManagmentSystem.Web/Program.cs
@@ -12,13 +12,12 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-//builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());          changed to:
+
+DataServicesRegistration.AddDataServices(builder.Services, builder.Configuration);
 ApplicationServicesRegistration.AddApplicationServices(builder.Services);
 
+//builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); 
 //builder.Services.AddScoped<ILeaveTypeServices, LeaveTypeServices>();
 //builder.Services.AddScoped<ILeaveAllocationService, LeaveAllocationService>();
 //builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
